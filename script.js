0ff36d8dfe41c57cff2650fa7a406fb84ff48452
selim <nfc.selim@gmail.com>
2023-02-04 23:01:04 +0100

    Schrift wird kleiner - kleines update

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function appendOperation(operation) {
 function calculateResult(){
     let container = document.getElementById('resultBox');
     let resultBox = eval(container.innerHTML);
-
     container.innerHTML = resultBox;
 }
 
@@ -17,3 +16,35 @@ function deleteLast(){
     container.innerHTML = container.innerHTML.slice(0, 0);
 }
 
+
+
+
+var div = document.getElementById("resultBox");
+var timerId;
+
+function checkLength() {
+  var textLength = div.innerText.length;
+
+  if(textLength >= 16){
+    div.style.fontSize = "20px";
+  }
+  else if (textLength >= 12) {
+    div.style.fontSize = "30px";
+  }
+  else if(textLength >= 11){
+    div.style.fontSize = "40px";
+  }
+  else if(textLength >= 8){
+    div.style.fontSize = "50px";
+  }
+  else if (textLength >= 7) {
+    div.style.fontSize = "60px";
+  }
+  else if(textLength >= 6){
+    div.style.fontSize = "70px";
+  } else {
+    div.style.fontSize = "80px";
+  }
+}
+
+timerId = setInterval(checkLength, 1000);
